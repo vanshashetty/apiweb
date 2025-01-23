beffa2cab2716fdb07069736866619be286c3f92
vamsha shetty <vanshashetty@yahoo.com>
2025-01-23 15:50:12 +1100

    Update HelloController.java
    prettify

--- src/main/java/com/APIweb/apiweb/HelloController.java
+++ src/main/java/com/APIweb/apiweb/HelloController.java
@@ -4,14 +4,15 @@ package com.APIweb.apiweb;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
+import com.fasterxml.jackson.databind.ObjectMapper; 
 
 @RestController
-public class HelloController {
+public class BooksController {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return new RestTemplate().getForObject("https://openlibrary.org/subjects/science_fiction.json?limit=5", String.class);
+    public String sayHello() throws Exception {
+        String response = new RestTemplate().getForObject("https://openlibrary.org/subjects/science_fiction.json?limit=5", String.class);
+        return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(new ObjectMapper().readTree(response)); // Pretty-print JSON
     }
 }
 
